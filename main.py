72eb9cc09e8955f8a72202e9593c5768c2e29d54
stephenCarmody <stephen.m.carmody@gmail.com>
2019-11-30 21:03:46 +0100

    added new function

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+# https://realpython.com/python-continuous-integration/
+
+
 def start():
-	print("Hello World")
+	print("Hello World, we have started")
+
+def count_list(in_list):
+	return len(in_list)
