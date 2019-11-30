623cf8120b7afb787c65eab613a88fb198539836
stephenCarmody <stephen.m.carmody@gmail.com>
2019-11-30 21:50:40 +0100

    fixed test error

--- test_main.py
+++ test_main.py
@@ -10,6 +10,5 @@ class TestMain:
     def test_count_list(self):
         assert 2 == main.count_list(['a', 'b'])
 
-
     def test_multiplication(self):
         assert 100 == main.multiply(10, 10)
