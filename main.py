7ee30957f9976381778b9d7ca9e3145b1225bfae
stephenCarmody <stephen.m.carmody@gmail.com>
2019-11-30 21:46:26 +0100

    added new funtion & test

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ def start():
 
 def count_list(in_list):
     return len(in_list)
+
+
+def multiply(first_term, second_term):
+    return first_term * second_term
--- test_main.py
+++ test_main.py
@@ -9,3 +9,7 @@ class TestMain:
 
     def test_count_list(self):
         assert 2 == main.count_list(['a', 'b'])
+
+
+    def test_multiplication(self):
+        assert 100 == calculator.multiply(10, 10)
